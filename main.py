acae6da484865575d97dbaf4ae508c13d3967793
yarden <yarden.tulchinsky@gmail.com>
2024-06-21 12:04:31 +0300

    getting a random number from 1 to 3 for a random letter each time, and get the letter and change the name in the letter to the one who has birthday

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ current_day = dt.datetime.now().day
 current_month = dt.datetime.now().month
 for birthday in birthdays_list:
     if current_day == birthday['day'] and current_month == birthday['month']:
-        print('its today')
+        random_letter = random.randint(1,3)
+
+        #get a random letter and change it to have the name of the one who has birthday
+        with open(f'letter_templates/letter_{random_letter}.txt','r') as letter:
+            wish = letter.read()
+            new_wish= wish.replace('[NAME]',birthday['name'].title())
