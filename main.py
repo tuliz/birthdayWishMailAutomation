99698417fe5a606e09678e2e791e9b8f2fcc5e79
yarden <yarden.tulchinsky@gmail.com>
2024-06-21 12:12:00 +0300

    after getting the letter ready start a new connection for smtp and send in email the birthday wish

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 import datetime as dt
 import pandas
 
+EMAIL = 'yarden.tulchinsky@gmail.com'
+PASSWORD = 'ecib yifv vrbb bjpz'
 birthdays_dataframe = pandas.read_csv('birthdays.csv')
 birthdays_list = birthdays_dataframe.to_dict(orient='records')
 
@@ -16,3 +18,8 @@ for birthday in birthdays_list:
         with open(f'letter_templates/letter_{random_letter}.txt','r') as letter:
             wish = letter.read()
             new_wish= wish.replace('[NAME]',birthday['name'].title())
+
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(EMAIL,PASSWORD)
+            connection.sendmail(EMAIL,birthday['email'],msg=f'subject:Happy Birthday\n\n{new_wish}')
